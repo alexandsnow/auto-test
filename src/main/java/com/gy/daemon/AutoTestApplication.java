75abac8a62e5542a99b5755a97cb816adb471835
GaoYang <1464911422@qq.com>
2017-09-28 23:35:22 +0800

    【task】create auto-test framework

---
+++ src/main/java/com/gy/daemon/AutoTestApplication.java
@@ -0,0 +1,12 @@
+package com.gy.daemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestApplication.class, args);
+	}
+}

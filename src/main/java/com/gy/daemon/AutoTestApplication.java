ec4b55e2bbcf6a67aadb8461503a92b489b33826
GaoYang <yang_gao@Ctrip.com>
2017-10-01 12:01:49 +0800

    【task】add a healthCheck interface

--- src/main/java/com/gy/daemon/AutoTestApplication.java
+++ src/main/java/com/gy/daemon/AutoTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutoTestApplication.class, args);
+		SpringApplication.run(Auto	TestApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/gy/daemon/controller/HealthCheck.java
@@ -0,0 +1,16 @@
+package com.gy.daemon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yang_gao on 2017/10/1.
+ */
+
+@RestController
+public class HealthCheck {
+    @GetMapping(value = "/")
+    public String healthState(){
+        return "auto-test project is running stably";
+    }
+}
